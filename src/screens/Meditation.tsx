8a7306d2c418b83379cbcbeac03050cd8c5f85dc
Sean Sassenrath <sean.sassenrath@gmail.com>
2022-11-10 15:42:27 -0800

    clean up meditation page

--- src/screens/Meditation.tsx
+++ src/screens/Meditation.tsx
@@ -28,17 +28,22 @@ const MeditationScreen = ({ route }: MeditationStackScreenProps<'Meditation'>) =
   return (
     <Layout style={styles.container}>
       <SafeAreaView style={styles.container}>
-        <Layout style={styles.container}>
-          <Layout style={styles.layout}>
-            <Button
-              appearance='ghost'
-              accessoryLeft={CloseIcon}
-              onPress={onClosePress}
-            />
-          </Layout>
-          <Layout style={styles.header}>
-            <Text category='h5'>{name}</Text>
-          </Layout>
+        <Layout style={styles.topBar}>
+          <Text category='h4' style={styles.topBarText}>{name}</Text>
+          <Button
+            appearance='ghost'
+            accessoryLeft={CloseIcon}
+            onPress={onClosePress}
+            style={styles.topBarIcon}
+          />
+        </Layout>
+        <Layout style={styles.mainSection}>
+          <Text category='s1'>Set intention</Text>
+          <Text category='s1'>Turn on do not disturb</Text>
+          <Text category='s1'>Take off watch</Text>
+        </Layout>
+        <Layout style={styles.bottomBar}>
+          <Button>Start</Button>
         </Layout>
       </SafeAreaView>
     </Layout>
@@ -46,6 +51,10 @@ const MeditationScreen = ({ route }: MeditationStackScreenProps<'Meditation'>) =
 }
 
 const styles = StyleSheet.create({
+  bottomBar: {
+    flex: 1,
+    padding: 20,
+  },
   container: {
     flex: 1,
   },
@@ -61,6 +70,23 @@ const styles = StyleSheet.create({
   layout: {
     flexDirection: 'row-reverse',
   },
+  mainSection: {
+    padding: 20,
+    flex: 6,
+  },
+  topBar: {
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    paddingLeft: 20,
+    paddingTop: 20,
+    flex: 1,
+  },
+  topBarText: {
+    flex: 9,
+  },
+  topBarIcon: {
+    flex: 1
+  }
 })
 
 export default MeditationScreen;
