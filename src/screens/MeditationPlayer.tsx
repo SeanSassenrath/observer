80f19e966e8751fe7b92a1b7f4a580fa37ef88fa
Sean Sassenrath <sean.sassenrath@gmail.com>
2022-11-11 15:14:17 -0800

    implement basic audio playback

--- src/screens/MeditationPlayer.tsx
+++ src/screens/MeditationPlayer.tsx
@@ -1,15 +1,61 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import TrackPlayer, { Capability  } from 'react-native-track-player';
 import { Button, Layout, Text } from '@ui-kitten/components';
 
-import { MeditationPlayerScreenNavigationProp } from '../types';
+import MeditationDataContext from '../contexts/meditationData';
+import { MeditationPlayerScreenNavigationProp, MeditationPlayerStackScreenProps } from '../types';
 
-const MeditationPlayer = () => {
+const track = {
+  title: 'testing',
+  url: require('../tracks/test.mp3'),
+  artist: 'dr joe',
+}
+
+const MeditationPlayer = ({ route }: MeditationPlayerStackScreenProps<'MeditationPlayer'>) => {
+  const { meditationFiles } = useContext(MeditationDataContext);
   const navigation = useNavigation<MeditationPlayerScreenNavigationProp>();
+  const { name } = route.params;
+
+  const meditationFile = meditationFiles.map(meditation => meditation.normalizedName === name);
+
+  const setupTrackPlayer = async () => {
+    try {
+      await TrackPlayer.setupPlayer();
+      await TrackPlayer.add(track)
+      TrackPlayer.updateOptions({
+        // Media controls capabilities
+        capabilities: [
+          Capability.Play,
+          Capability.Pause,
+          Capability.SkipToNext,
+          Capability.SkipToPrevious,
+          Capability.Stop,
+        ],
+
+        // Capabilities that will show up when the notification is in the compact form on Android
+        compactCapabilities: [Capability.Play, Capability.Pause],
+      })
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    setupTrackPlayer();
+  }, []);
 
   const onClosePress = () => {
     navigation.pop();
+  }
+
+  const onPlayPress = () => {
+    TrackPlayer.play();
+  }
+
+  const onPausePress = () => {
+    TrackPlayer.pause();
   }
 
   return (
@@ -18,6 +64,8 @@ const MeditationPlayer = () => {
         <Layout style={styles.topBar}>
           <Text category='h5' style={styles.topBarText}>Meditation Player</Text>
           <Button onPress={onClosePress}>Close</Button>
+          <Button onPress={onPlayPress}>Play</Button>
+          <Button onPress={onPausePress}>Pause</Button>
         </Layout>
       </SafeAreaView>
     </Layout>
