067114dee6625366c8e7fcdccd2251a08cdefa62
Sean Sassenrath <sean.sassenrath@gmail.com>
2022-11-27 10:16:19 -0800

    experimenting with orb animation

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,7 +2,8 @@ import React, { useContext, useEffect } from 'react';
 import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import _ from 'lodash';
-import { Card, Layout, Text } from '@ui-kitten/components'; 
+import { Card, Layout, Text, withStyles } from '@ui-kitten/components'; 
+import Animated, { Easing, useAnimatedStyle, useSharedValue, withRepeat, withTiming } from 'react-native-reanimated';
 
 import _Button from '../components/Button';
 import { MeditationScreenNavigationProp, MeditationId, LibraryScreenNavigationProp } from '../types';
@@ -15,6 +16,17 @@ const HomeScreen = () => {
   const { recentMeditationIds } = useContext(RecentMeditationIdsContext);
   const stackNavigation = useNavigation<MeditationScreenNavigationProp>();
   const tabNavigation = useNavigation<LibraryScreenNavigationProp>();
+  const naviTransformY = useSharedValue(0);
+
+  useEffect(() => {
+    naviTransformY.value = withRepeat(withTiming(-45, { duration: 5000, easing: Easing.inOut(Easing.ease)}), -1, true)
+  }, []);
+
+  const naviReanimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateY: naviTransformY.value }]
+    }
+  })
 
   const onMeditationClick = (meditationId: MeditationId) => {
     if (meditationId) {
@@ -57,7 +69,17 @@ const HomeScreen = () => {
     <Layout style={styles.container}>
       <ScrollView style={styles.scrollContainer}>
         <SafeAreaView style={styles.container}>
-          <HomeTopBar onVoidPress={onVoidPress}/>
+          <HomeTopBar onVoidPress={onVoidPress} />
+          <Layout style={styles.orbContainer}>
+            <Animated.View
+              style={[styles.navi, naviReanimatedStyle]}
+            >
+              <Layout style={[styles.orb2]} />
+              <Animated.View
+                style={[styles.orb]}
+              />
+            </Animated.View>
+          </Layout>
           <HomeStreaks />
           <Layout>
             {/* <MeditationList
@@ -101,6 +123,40 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column'
   },
+  orbContainer: {
+    height: 200,
+    backgroundColor: 'black',
+    marginBottom: 40,
+  },
+  navi: {
+    height: 200,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  orb: {
+    height: 70,
+    width: 70,
+    backgroundColor: '#D5EDF5',
+    borderRadius: 45,
+    borderColor: 'rgba(160, 139, 247, 0.7)',
+    borderWidth: 3,
+    shadowColor: 'rgba(160, 139, 247, 1)',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 1,
+    shadowRadius: 20,
+    opacity: .9,
+    marginTop: -70,
+  },
+  orb2: {
+    height: 70,
+    width: 70,
+    // backgroundColor: '#D5EDF5',
+    borderRadius: 45,
+    shadowColor: 'rgba(250, 232, 130, 0.8)',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 1,
+    shadowRadius: 40,
+  }
 })
 
 export default HomeScreen;
