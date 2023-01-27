9372429fc3cde4d36d00cda66f1d4545fe4e7d27
Sean Sassenrath <sean.sassenrath@gmail.com>
2023-01-26 20:46:58 -0800

    add pagination for meditation history

--- src/screens/Insight.tsx
+++ src/screens/Insight.tsx
@@ -1,44 +1,124 @@
-import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
-import { Avatar, Card, Layout, List, Text, useStyleSheet } from '@ui-kitten/components';
+import React, { useContext, useEffect, useState } from 'react';
+import { FlatList, SafeAreaView, StyleSheet } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import { Layout, Text, useStyleSheet } from '@ui-kitten/components';
 
-import { HomeStreaks } from '../components/HomeStreaks';
-import { MeditationTimeLog } from '../components/MeditationTimeLog';
 import { TopMeditations } from '../components/TopMeditations';
-import { MeditationHistory } from '../components/MeditationHistory';
+import { MeditationInstance } from '../types';
+import UserContext from '../contexts/userData';
+import { DateTime } from 'luxon';
+
+const EMPTY_STRING = '';
 
 const InsightScreen = () => {
   const styles = useStyleSheet(themedStyles);
+  const { user } = useContext(UserContext);
+  const [meditationHistory, setMeditationHistory] = useState([] as MeditationInstance[]);
+  const [lastBatchDocument, setLastBatchDocument] = useState();
+  const [hasNoMoreHistory, setHasNoMoreHistory] = useState(false);
+
+  useEffect(() => {
+    firestore()
+      .collection('users')
+      .doc(user.uid)
+      .collection('meditationHistory')
+      .orderBy('creationTime', 'desc')
+      .limit(20)
+      .get()
+      .then(meditationInstances => {
+        const docs = meditationInstances.docs;
+        setLastBatchDocument(docs[docs.length - 1] as any)
+        const meditationHistoryFromFirebase = docs.map(doc => doc.data());
+        setMeditationHistory(meditationHistoryFromFirebase as MeditationInstance[]);
+      })
+  }, [])
+
+  const fetchMoreMeditationData = () => {
+    if (hasNoMoreHistory) { return; }
+
+    firestore()
+      .collection('users')
+      .doc(user.uid)
+      .collection('meditationHistory')
+      .orderBy('creationTime', 'desc')
+      .startAfter(lastBatchDocument)
+      .limit(20)
+      .get()
+      .then(meditationInstances => {
+        const docs = meditationInstances.docs;
+
+        if (docs.length <= 0) {
+          setHasNoMoreHistory(true);
+        }
+      
+        setLastBatchDocument(docs[docs.length - 1] as any)
+        const meditationHistoryFromFirebase = docs.map(doc => doc.data());
+        setMeditationHistory([...meditationHistory, ...meditationHistoryFromFirebase] as MeditationInstance[]);
+      })
+  }
+
+  const getDisplayDate = (item: MeditationInstance) => {
+    if (item.creationTime) {
+      const date = DateTime.fromSeconds(item.creationTime.seconds);
+      return date.toLocaleString(DateTime.DATETIME_SHORT);
+    } else {
+      return EMPTY_STRING;
+    }
+  }
 
   const renderListItem = ({ item, index }: any) => {
-    const level = index % 2 ? '2' : '4';
+    const level = index % 2 ? '2' : '1';
+    const displayDate = getDisplayDate(item);
+    const isFirstItem = index === 0;
+    const isLastItem = (meditationHistory.length - 1) === index;
+    const itemStyles = isFirstItem
+      ? styles.firstItem
+      : isLastItem
+        ? styles.lastItem
+        : styles.listItem;
 
     return (
-      <Layout style={styles.listItem} level={level}>
+      <Layout style={itemStyles} level={level} key={index}>
         <Layout level={level} style={styles.listItemDataContainer}>
           <Layout level={level}>
-            <Text category='s1' style={styles.listItemText}>{item.date}</Text>
-            {/* <Text category='s2' style={styles.listItemText}>{item.time}</Text> */}
             <Text category='s2' style={styles.listItemText}>{item.name}</Text>
+            <Text category='s2' style={styles.listItemText}>{displayDate}</Text>
           </Layout>
-          <Layout level={level}>
-            <Layout style={styles.listItemIndicator}/>
-          </Layout>
+          <Layout style={styles.listItemIndicator} />
         </Layout>
       </Layout>
     )
   }
 
+  const renderHeader = () => (
+    <Layout level = '4'>
+      <Layout level='4' style={styles.topSpacer}>
+        <TopMeditations />
+      </Layout>
+      <Layout level='4'>
+        <Text category='h6' style={styles.header}>Meditation History</Text>
+      </Layout>
+    </Layout>
+  )
+
+  const renderFooter = () => (
+    <Layout
+      level='4'
+      style={styles.footer}
+    />
+  )
+
   return (
     <Layout style={styles.rootContainer} level='4'>
       <SafeAreaView style={styles.screenContainer}>
-        <ScrollView style={styles.scrollContainer}>
-            <Layout level='4' style={styles.topSpacer} />
-            {/* <HomeStreaks /> */}
-            {/* <MeditationTimeLog /> */}
-            <TopMeditations />
-            <MeditationHistory />
-        </ScrollView>
+        <FlatList
+          data={meditationHistory}
+          renderItem={({ item, index }) => renderListItem({ item, index })}
+          onEndReached={fetchMoreMeditationData}
+          onEndReachedThreshold={0.8}
+          ListHeaderComponent={renderHeader()}
+          ListFooterComponent={renderFooter()}
+        />
       </SafeAreaView>
     </Layout>
   )
@@ -66,6 +146,7 @@ const themedStyles = StyleSheet.create({
   listItem: {
     paddingVertical: 16,
     paddingHorizontal: 20,
+    marginHorizontal: 20,
   },
   listItemText: {
     marginVertical: 2,
@@ -99,7 +180,7 @@ const themedStyles = StyleSheet.create({
     marginBottom: 8,
   },
   topSpacer: {
-    margin: 30,
+    marginTop: 40,
   },
   timeContainer: {
     marginHorizontal: 20,
@@ -113,11 +194,30 @@ const themedStyles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  timePeriod: {
-
+  header: {
+    opacity: 0.8,
+    paddingHorizontal: 20,
+    paddingBottom: 16,
   },
   timeAmount: {
     marginTop: 16,
+  },
+  firstItem: {
+    paddingVertical: 16,
+    paddingHorizontal: 20,
+    marginHorizontal: 20,
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+  },
+  lastItem: {
+    paddingVertical: 16,
+    paddingHorizontal: 20,
+    marginHorizontal: 20,
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+  },
+  footer: {
+    paddingVertical: 20,
   }
 })
 
