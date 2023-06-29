886447d622792e4d7291c42c5e5f21d815e454e5
Sean Sassenrath <sean.sassenrath@gmail.com>
2023-06-28 21:41:45 -0700

    format time in meditation chart

--- src/components/TimeInMeditationChart.tsx
+++ src/components/TimeInMeditationChart.tsx
@@ -1,23 +1,21 @@
-import React, { useContext } from 'react';
-import { Dimensions, StyleSheet } from 'react-native';
-import {
-  LineChart,
-} from 'react-native-chart-kit'
-import { DateTime } from 'luxon';
-import { Layout, Text, useStyleSheet } from '@ui-kitten/components';
+import React from 'react';
+import {Dimensions, StyleSheet} from 'react-native';
+import {LineChart} from 'react-native-chart-kit';
+import {DateTime} from 'luxon';
+import {Layout, Text, useStyleSheet} from '@ui-kitten/components';
 
-import { MeditationInstance } from '../types';
+import {MeditationInstance} from '../types';
 
 const makeMeditationTimeData = (meditationHistory: MeditationInstance[]) => {
   const meditationTimeData = [0, 0, 0, 0, 0];
 
-  const dt = DateTime.now();
-  const day0 = dt.toLocaleString();
-  const day1 = dt.minus({ days: 1 }).toLocaleString();
-  const day2 = dt.minus({ days: 2 }).toLocaleString();
-  const day3 = dt.minus({ days: 3 }).toLocaleString();
-  const day4 = dt.minus({ days: 4 }).toLocaleString();
-  const day5 = dt.minus({ days: 5 }).toLocaleString();
+  const now = DateTime.now();
+  const day0 = now.toLocaleString();
+  const day1 = now.minus({days: 1}).toLocaleString();
+  const day2 = now.minus({days: 2}).toLocaleString();
+  const day3 = now.minus({days: 3}).toLocaleString();
+  const day4 = now.minus({days: 4}).toLocaleString();
+  const day5 = now.minus({days: 5}).toLocaleString();
 
   meditationHistory.forEach((meditation: MeditationInstance) => {
     if (!meditation || !meditation.meditationStartTime) {
@@ -26,10 +24,7 @@ const makeMeditationTimeData = (meditationHistory: MeditationInstance[]) => {
     const dt = DateTime.fromSeconds(meditation.meditationStartTime);
     const meditationLocaleString = dt && dt.toLocaleString();
 
-    if (
-      !meditationLocaleString ||
-      meditationLocaleString === day5
-    ) {
+    if (!meditationLocaleString || meditationLocaleString === day5) {
       return;
     }
 
@@ -59,43 +54,49 @@ const makeMeditationTimeData = (meditationHistory: MeditationInstance[]) => {
       const dayTime = dayCurrentTime + timeMeditated;
       meditationTimeData[4] = dayTime;
     }
-  })
+  });
 
   return meditationTimeData;
-}
+};
 
 interface TimeInMeditationChartProps {
-  meditationHistory: MeditationInstance[],
-  style: any,
+  meditationHistory: MeditationInstance[];
+  style: any;
 }
 
 export const TimeInMeditationChart = (props: TimeInMeditationChartProps) => {
   const styles = useStyleSheet(themedStyles);
-  
+
   const meditationTimeData = makeMeditationTimeData(props.meditationHistory);
 
   const day0 = DateTime.now();
-  const day1 = day0.minus({ days: 1 });
-  const day2 = day0.minus({ days: 2 });
-  const day3 = day0.minus({ days: 3 });
-  const day4 = day0.minus({ days: 4 });
+  const day1 = day0.minus({days: 1});
+  const day2 = day0.minus({days: 2});
+  const day3 = day0.minus({days: 3});
+  const day4 = day0.minus({days: 4});
 
   const data = {
-    labels: [day4.weekdayShort, day3.weekdayShort, day2.weekdayShort, day1.weekdayShort, day0.weekdayShort],
+    labels: [
+      day4.weekdayShort,
+      day3.weekdayShort,
+      day2.weekdayShort,
+      day1.weekdayShort,
+      day0.weekdayShort,
+    ],
     datasets: [
       {
-        data: meditationTimeData.reverse().map((seconds) => seconds / 60),
+        data: meditationTimeData.reverse().map(seconds => seconds / 60),
         color: (opacity = 1) => `rgba(119,205,72, ${opacity})`,
-        strokeWidth: 2
-      }
+        strokeWidth: 2,
+      },
     ],
-    legend: ["Minutes"]
+    legend: ['Minutes'],
   };
 
   const chartConfig = {
-    backgroundGradientFrom: "rgba(26, 33, 56, 1)",
+    backgroundGradientFrom: 'rgba(26, 33, 56, 1)',
     backgroundGradientFromOpacity: 1,
-    backgroundGradientTo: "rgba(26, 33, 56, 1)",
+    backgroundGradientTo: 'rgba(26, 33, 56, 1)',
     backgroundGradientToOpacity: 1,
     color: (opacity = 1) => `rgba(225, 225, 225, ${opacity})`,
     decimalPlaces: 0,
@@ -104,27 +105,29 @@ export const TimeInMeditationChart = (props: TimeInMeditationChartProps) => {
     useShadowColorFromDataset: false,
   };
 
-  const screenWidth = Dimensions.get("window").width-40;
+  const screenWidth = Dimensions.get('window').width - 40;
 
   return (
-    <Layout level='4' style={props.style}>
-      <Text category='h6' style={styles.header}>Time in Meditation</Text>
-        <Layout level='2' style={styles.chartContainer}>
-          <LineChart
-            data={data}
-            width={screenWidth}
-            height={220}
-            chartConfig={chartConfig}
-            fromZero
-            style={{
-              borderRadius: 16,
-              marginHorizontal: 20,
-            }}
-          />
-        </Layout>
+    <Layout level="4" style={props.style}>
+      <Text category="h6" style={styles.header}>
+        Time in Meditation
+      </Text>
+      <Layout level="2" style={styles.chartContainer}>
+        <LineChart
+          data={data}
+          width={screenWidth}
+          height={220}
+          chartConfig={chartConfig}
+          fromZero
+          style={{
+            borderRadius: 16,
+            marginHorizontal: 20,
+          }}
+        />
+      </Layout>
     </Layout>
-  )
-}
+  );
+};
 
 const themedStyles = StyleSheet.create({
   chartContainer: {
@@ -160,5 +163,5 @@ const themedStyles = StyleSheet.create({
   topMeditationName: {
     paddingRight: 6,
     flex: 1,
-  }
-})
+  },
+});
