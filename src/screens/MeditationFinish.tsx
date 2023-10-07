ba93dd0087cc4d30dc4ae11f2c5d4c373221069c
Sean Sassenrath <sean.sassenrath@gmail.com>
2023-10-07 13:48:02 -0700

    prettier meditation finish

--- src/screens/MeditationFinish.tsx
+++ src/screens/MeditationFinish.tsx
@@ -1,15 +1,17 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { Layout, Text } from '@ui-kitten/components';
-import firestore, { FirebaseFirestoreTypes } from '@react-native-firebase/firestore';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import React, {useContext, useEffect, useState} from 'react';
+import {StyleSheet} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {Layout, Text} from '@ui-kitten/components';
+import firestore, {
+  FirebaseFirestoreTypes,
+} from '@react-native-firebase/firestore';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import _ from 'lodash';
 
 import _Button from '../components/Button';
-import { MultiLineInput } from '../components/MultiLineInput';
+import {MultiLineInput} from '../components/MultiLineInput';
 import MeditationInstanceDataContext from '../contexts/meditationInstanceData';
-import UserContext, { User } from '../contexts/userData';
+import UserContext from '../contexts/userData';
 import {
   getLastMeditationFromMeditationHistory,
   makeUpdatedBreathMeditationCountData,
@@ -18,43 +20,60 @@ import {
   makeUpdatedMeditationCountData,
   makeUpdatedRecentUserMeditationData,
 } from '../utils/meditation';
-import { getUserStreakData, makeUpdatedStreakData, UpdatedStreakData } from '../utils/streaks';
+import {
+  getUserStreakData,
+  makeUpdatedStreakData,
+  UpdatedStreakData,
+} from '../utils/streaks';
 import MeditationHistoryContext from '../contexts/meditationHistory';
-import { fbUpdateUser } from '../fb/user';
-import { fbAddMeditationHistory, fbUpdateMeditationHistory } from '../fb/meditationHistory';
-import { StreakUpdate } from '../components/StreakUpdate';
-import { Action, Noun, thinkboxSendEvent } from '../analytics';
+import {fbUpdateUser} from '../fb/user';
+import {
+  fbAddMeditationHistory,
+  fbUpdateMeditationHistory,
+} from '../fb/meditationHistory';
+import {StreakUpdate} from '../components/StreakUpdate';
+import {Action, Noun, thinkboxSendEvent} from '../analytics';
 
 const EMPTY_INPUT = '';
 
 const MeditationFinishScreen = () => {
   const navigation = useNavigation();
-  const { meditationInstanceData } = useContext(MeditationInstanceDataContext);
-  const { meditationHistory, setMeditationHistory } = useContext(MeditationHistoryContext);
-  const { user, setUser } = useContext(UserContext);
-  const [firstInput, setFirstInput] = useState(EMPTY_INPUT)
-  const [secondInput, setSecondInput] = useState(EMPTY_INPUT)
-  const [meditationInstanceDoc, setMeditationInstanceDoc] = useState({} as FirebaseFirestoreTypes.DocumentReference<FirebaseFirestoreTypes.DocumentData>);
-  const [updatedStreaksData, setUpdatedStreaksData] = useState({} as UpdatedStreakData);
+  const {meditationInstanceData} = useContext(MeditationInstanceDataContext);
+  const {meditationHistory, setMeditationHistory} = useContext(
+    MeditationHistoryContext,
+  );
+  const {user, setUser} = useContext(UserContext);
+  const [firstInput, setFirstInput] = useState(EMPTY_INPUT);
+  const [secondInput, setSecondInput] = useState(EMPTY_INPUT);
+  const [meditationInstanceDoc, setMeditationInstanceDoc] = useState(
+    {} as FirebaseFirestoreTypes.DocumentReference<FirebaseFirestoreTypes.DocumentData>,
+  );
+  const [updatedStreaksData, setUpdatedStreaksData] = useState(
+    {} as UpdatedStreakData,
+  );
 
   useEffect(() => {
-    thinkboxSendEvent(
-      Action.VIEW,
-      Noun.ON_MOUNT,
-      {
-        meditationName: meditationInstanceData.name,
-        meditationBaseId: meditationInstanceData.meditationBaseId,
-      },
-    );
+    thinkboxSendEvent(Action.VIEW, Noun.ON_MOUNT, {
+      meditationName: meditationInstanceData.name,
+      meditationBaseId: meditationInstanceData.meditationBaseId,
+    });
     updateUserMeditationData();
     addFbMeditationInstance();
-  }, [])
+  }, []);
 
   const updateUserMeditationData = async () => {
-    const updatedMeditationInstanceCount = makeUpdatedMeditationCountData(user, meditationInstanceData);
-    const updatedBreathMeditationCountData = makeUpdatedBreathMeditationCountData(user, meditationInstanceData);
-    const updatedRecentUserMeditationData = makeUpdatedRecentUserMeditationData(user, meditationInstanceData);
-    const lastMeditation = getLastMeditationFromMeditationHistory(meditationHistory);
+    const updatedMeditationInstanceCount = makeUpdatedMeditationCountData(
+      user,
+      meditationInstanceData,
+    );
+    const updatedBreathMeditationCountData =
+      makeUpdatedBreathMeditationCountData(user, meditationInstanceData);
+    const updatedRecentUserMeditationData = makeUpdatedRecentUserMeditationData(
+      user,
+      meditationInstanceData,
+    );
+    const lastMeditation =
+      getLastMeditationFromMeditationHistory(meditationHistory);
     const updatedStreaksData = makeUpdatedStreakData(user, lastMeditation);
     setUpdatedStreaksData(updatedStreaksData);
 
@@ -66,10 +85,7 @@ const MeditationFinishScreen = () => {
       meditationInstanceData,
     );
 
-    await fbUpdateUser(
-      user.uid,
-      updatedFbUserMeditationData,
-    );
+    await fbUpdateUser(user.uid, updatedFbUserMeditationData);
 
     const updatedContextMeditationData = makeUpdatedContextMeditationData(
       updatedMeditationInstanceCount,
@@ -81,7 +97,7 @@ const MeditationFinishScreen = () => {
     );
 
     setUser(updatedContextMeditationData);
-  }
+  };
 
   const addFbMeditationInstance = async () => {
     const doc = await fbAddMeditationHistory(user.uid, {
@@ -91,78 +107,80 @@ const MeditationFinishScreen = () => {
     if (doc && !Object.keys(meditationInstanceDoc).length) {
       setMeditationInstanceDoc(doc);
     }
-  }
+  };
 
   const updateFbMeditationInstance = async () => {
     const updatedMeditationInstanceData = {
       ...meditationInstanceData,
       notes: firstInput,
       feedback: secondInput,
-    }
+    };
     const meditationInstanceId = meditationInstanceDoc.id;
 
     if (meditationInstanceId) {
       await fbUpdateMeditationHistory(
         user.uid,
         meditationInstanceId,
-        updatedMeditationInstanceData
+        updatedMeditationInstanceData,
       );
     }
     // TODO: Add error toast here
-  }
+  };
 
   const updateMeditationHistoryContext = () => {
-    const currentMeditationHistory = meditationHistory.meditationInstances || [];
-    const updatedMeditationHistory = [meditationInstanceData].concat(currentMeditationHistory);
-    setMeditationHistory({ meditationInstances: updatedMeditationHistory })
-  }
+    const currentMeditationHistory =
+      meditationHistory.meditationInstances || [];
+    const updatedMeditationHistory = [meditationInstanceData].concat(
+      currentMeditationHistory,
+    );
+    setMeditationHistory({meditationInstances: updatedMeditationHistory});
+  };
 
   const onDonePress = () => {
     updateFbMeditationInstance();
     updateMeditationHistoryContext();
     //@ts-ignore
-    navigation.navigate('TabNavigation', { screen: 'Insight' });
-  }
+    navigation.navigate('TabNavigation', {screen: 'Insight'});
+  };
 
   const streaks = getUserStreakData(user);
 
   return (
     <KeyboardAwareScrollView style={styles.scrollContainer}>
-      <Layout style={styles.rootContainer} level='4'>
-        <Text category='h5' style={styles.text}>Thinkbox</Text>
-        {updatedStreaksData.streakUpdated
-          ? <StreakUpdate
-              current={streaks.current}
-              longest={streaks.longest}
-            />
-          : null
-        }
-        <Layout level='4' style={styles.inputContainer}>
-          <Text category='s1' style={styles.smallText}>What did you do well in your last meditation?</Text>
+      <Layout style={styles.rootContainer} level="4">
+        <Text category="h5" style={styles.text}>
+          Thinkbox
+        </Text>
+        {updatedStreaksData.streakUpdated ? (
+          <StreakUpdate current={streaks.current} longest={streaks.longest} />
+        ) : null}
+        <Layout level="4" style={styles.inputContainer}>
+          <Text category="s1" style={styles.smallText}>
+            What did you do well in your last meditation?
+          </Text>
           <MultiLineInput
             onChangeText={setFirstInput}
-            placeholder='Add what you did well here'
+            placeholder="Add what you did well here"
             value={firstInput}
           />
         </Layout>
-        <Layout level='4' style={styles.inputContainer}>
-          <Text category='s1' style={styles.smallText}>If you had another opportunity, what would you do differently?</Text>
+        <Layout level="4" style={styles.inputContainer}>
+          <Text category="s1" style={styles.smallText}>
+            If you had another opportunity, what would you do differently?
+          </Text>
           <MultiLineInput
             onChangeText={setSecondInput}
-            placeholder='Add what you would do differently here'
+            placeholder="Add what you would do differently here"
             value={secondInput}
           />
         </Layout>
-        <_Button
-          onPress={onDonePress}
-          style={styles.doneButton}
-        >
+        <_Button onPress={onDonePress} style={styles.doneButton}>
           DONE
         </_Button>
       </Layout>
     </KeyboardAwareScrollView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   bottomBarContainer: {
@@ -196,7 +214,7 @@ const styles = StyleSheet.create({
   },
   description: {
     marginBottom: 20,
-  }
-})
+  },
+});
 
 export default MeditationFinishScreen;
