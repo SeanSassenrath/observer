213e5100cad325ecc68ef4e46048919755fc7f78
Sean Sassenrath <sean.sassenrath@gmail.com>
2023-10-14 11:40:44 -0700

    update streak test

--- src/utils/streaks.test.ts
+++ src/utils/streaks.test.ts
@@ -1,37 +1,40 @@
-import { DateTime } from 'luxon';
-import { initialUserState } from '../contexts/userData';
-import { MeditationInstance, MeditationTypes } from '../types';
+import {DateTime} from 'luxon';
+import {initialUserState} from '../contexts/userData';
+import {MeditationInstance, MeditationTypes} from '../types';
 
-import { checkStreakData, makeUpdatedStreakData } from './streaks';
+import {checkStreakData, makeUpdatedStreakData} from './streaks';
 
 const mockMeditationInstance = {
   meditationBaseId: '',
   name: '',
   type: MeditationTypes.Meditation,
-}
+};
 
 describe('Initial Current Streak State', () => {
-  test(`It should not reset if the user's last meditation was started yesterday`, () => {
+  test("It should not reset if the user's last meditation was started yesterday", () => {
     const currentStreak = 1;
     const longestStreak = 3;
     const dt = DateTime.now();
-    const yesterdayInSeconds = dt.minus({ days: 1 }).toSeconds();
+    const yesterdayInSeconds = dt.minus({days: 1}).toSeconds();
     const lastMeditation: MeditationInstance = {
       ...mockMeditationInstance,
       meditationStartTime: yesterdayInSeconds,
-    }
+    };
 
     const originalStreakValues = {
       current: currentStreak,
       longest: longestStreak,
-    }
+    };
 
-    const updatedStreakValues = checkStreakData(originalStreakValues, lastMeditation);
+    const updatedStreakValues = checkStreakData(
+      originalStreakValues,
+      lastMeditation,
+    );
 
     expect(updatedStreakValues).toEqual(originalStreakValues);
   });
 
-  test(`It should not reset if the user's last meditation was started today`, () => {
+  test("It should not reset if the user's last meditation was started today", () => {
     const currentStreak = 1;
     const longestStreak = 3;
     const dt = DateTime.now();
@@ -39,41 +42,47 @@ describe('Initial Current Streak State', () => {
     const lastMeditation: MeditationInstance = {
       ...mockMeditationInstance,
       meditationStartTime: todayInSeconds,
-    }
+    };
 
     const originalStreakValues = {
       current: currentStreak,
       longest: longestStreak,
-    }
+    };
 
-    const updatedStreakValues = checkStreakData(originalStreakValues, lastMeditation);
+    const updatedStreakValues = checkStreakData(
+      originalStreakValues,
+      lastMeditation,
+    );
 
     expect(updatedStreakValues).toEqual(originalStreakValues);
   });
 
-  test(`It should reset the streak count if the user's last meditation was started over a day ago`, () => {
+  test("It should reset the streak count if the user's last meditation was started over a day ago", () => {
     const currentStreak = 1;
     const longestStreak = 3;
     const dt = DateTime.now();
-    const twoDaysAgoInSeconds = dt.minus({ days: 2 }).toSeconds();
+    const twoDaysAgoInSeconds = dt.minus({days: 2}).toSeconds();
     const lastMeditation: MeditationInstance = {
       ...mockMeditationInstance,
       meditationStartTime: twoDaysAgoInSeconds,
-    }
+    };
 
     const originalStreakValues = {
       current: currentStreak,
       longest: longestStreak,
-    }
+    };
 
-    const updatedStreakValues = checkStreakData(originalStreakValues, lastMeditation);
+    const updatedStreakValues = checkStreakData(
+      originalStreakValues,
+      lastMeditation,
+    );
 
     expect(updatedStreakValues).not.toEqual(originalStreakValues);
   });
 });
 
 describe('Post Medtiation Updated Streak State', () => {
-  test(`It should not increase streak count if the last meditation was completed today`, () => {
+  test('It should not increase streak count if the last meditation was completed today', () => {
     const currentStreak = 1;
     const longestStreak = 3;
     const dt = DateTime.now();
@@ -81,14 +90,14 @@ describe('Post Medtiation Updated Streak State', () => {
     const lastMeditation: MeditationInstance = {
       ...mockMeditationInstance,
       meditationStartTime: todayInSeconds,
-    }
+    };
 
     const expectedStreakValues = {
       current: currentStreak,
       longest: longestStreak,
       newLongestStreak: false,
       streakUpdated: false,
-    }
+    };
 
     const user = {
       ...initialUserState,
@@ -96,31 +105,31 @@ describe('Post Medtiation Updated Streak State', () => {
         streaks: {
           current: currentStreak,
           longest: longestStreak,
-        }
-      }
-    }
+        },
+      },
+    };
 
     const updatedStreakValues = makeUpdatedStreakData(user, lastMeditation);
 
     expect(updatedStreakValues).toEqual(expectedStreakValues);
   });
 
-  test(`It should increase streak count if the last meditation was completed yesterday`, () => {
+  test('It should increase streak count if the last meditation was completed yesterday', () => {
     const currentStreak = 1;
     const longestStreak = 3;
     const dt = DateTime.now();
-    const yesterdayInSeconds = dt.minus({ days: 1 }).toSeconds();
+    const yesterdayInSeconds = dt.minus({days: 1}).toSeconds();
     const lastMeditation: MeditationInstance = {
       ...mockMeditationInstance,
       meditationStartTime: yesterdayInSeconds,
-    }
+    };
 
     const expectedStreakValues = {
       current: currentStreak + 1,
       longest: longestStreak,
       newLongestStreak: false,
       streakUpdated: true,
-    }
+    };
 
     const user = {
       ...initialUserState,
@@ -128,31 +137,31 @@ describe('Post Medtiation Updated Streak State', () => {
         streaks: {
           current: currentStreak,
           longest: longestStreak,
-        }
-      }
-    }
+        },
+      },
+    };
 
     const updatedStreakValues = makeUpdatedStreakData(user, lastMeditation);
 
     expect(updatedStreakValues).toEqual(expectedStreakValues);
   });
 
-  test(`It should increase streak count if the last meditation was completed yesterday`, () => {
+  test('It should increase longest and current streak count if the last meditation was completed yesterday', () => {
     const currentStreak = 3;
     const longestStreak = 3;
     const dt = DateTime.now();
-    const yesterdayInSeconds = dt.minus({ days: 1 }).toSeconds();
+    const yesterdayInSeconds = dt.minus({days: 1}).toSeconds();
     const lastMeditation: MeditationInstance = {
       ...mockMeditationInstance,
       meditationStartTime: yesterdayInSeconds,
-    }
+    };
 
     const expectedStreakValues = {
       current: currentStreak + 1,
       longest: longestStreak + 1,
       newLongestStreak: true,
       streakUpdated: true,
-    }
+    };
 
     const user = {
       ...initialUserState,
@@ -160,9 +169,9 @@ describe('Post Medtiation Updated Streak State', () => {
         streaks: {
           current: currentStreak,
           longest: longestStreak,
-        }
-      }
-    }
+        },
+      },
+    };
 
     const updatedStreakValues = makeUpdatedStreakData(user, lastMeditation);
 
