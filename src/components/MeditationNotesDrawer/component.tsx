1f06e57227b8676da248d3c7f994646fbf4bdc9b
Sean Sassenrath <sean.sassenrath@gmail.com>
2024-01-03 21:09:26 -0800

    MeditationNotesDrawer: Remove dedicated empty message

--- src/components/MeditationNotesDrawer/component.tsx
+++ src/components/MeditationNotesDrawer/component.tsx
@@ -49,38 +49,30 @@ export const MeditationNotesDrawerComponent = (props: Props) => {
         </View>
         <ScrollView style={styles.scrollViewContainer}>
           <View style={styles.textSectionsContainer}>
-            {meditationInstance?.intention ? (
-              <View style={styles.textSection}>
-                <Text category="s1" style={styles.textHeader}>
-                  Intention
-                </Text>
-                <Text category="h6">{meditationInstance?.intention}</Text>
-              </View>
-            ) : null}
-            {meditationInstance?.notes ? (
-              <View style={styles.textSection}>
-                <Text category="s1" style={styles.textHeader}>
-                  What did you do well in your meditation?
-                </Text>
-                <Text category="h6">{meditationInstance?.notes}</Text>
-              </View>
-            ) : null}
-            {meditationInstance?.feedback ? (
-              <View style={styles.textSection}>
-                <Text category="s1" style={styles.textHeader}>
-                  If you had another opportunity, what would you do differently?
-                </Text>
-                <Text category="h6">{meditationInstance?.feedback}</Text>
-              </View>
-            ) : null}
-            {meditationInstance?.feedback ||
-            meditationInstance?.intention ||
-            meditationInstance?.notes ? null : (
-              <Text category="h6" style={styles.emptyNotes}>
-                Add an intention, what you did well, and feedback to your
-                meditations to see it in your meditation notes.
+            <View style={styles.textSection}>
+              <Text category="s1" style={styles.textHeader}>
+                Intention
               </Text>
-            )}
+              <Text category="h6">
+                {meditationInstance?.intention || 'Not recorded'}
+              </Text>
+            </View>
+            <View style={styles.textSection}>
+              <Text category="s1" style={styles.textHeader}>
+                What did you do well in your meditation?
+              </Text>
+              <Text category="h6">
+                {meditationInstance?.notes || 'Not recorded'}
+              </Text>
+            </View>
+            <View style={styles.textSection}>
+              <Text category="s1" style={styles.textHeader}>
+                If you had another opportunity, what would you do differently?
+              </Text>
+              <Text category="h6">
+                {meditationInstance?.feedback || 'Not recorded'}
+              </Text>
+            </View>
           </View>
         </ScrollView>
         <View style={styles.buttonContainer}>
@@ -143,10 +135,6 @@ const styles = StyleSheet.create({
   startButton: {
     borderRadius: 100,
     marginBottom: 10,
-  },
-  emptyNotes: {
-    textAlign: 'center',
-    lineHeight: 30,
   },
   buttonContainer: {
     paddingHorizontal: 20,
